4db3392a6e446c0094c7ad2db8a09d61712bf833
Ivan Acosta-Rubio <ivan@bakedweb.net>
2009-02-02 13:09:19 -0500

    adding admin

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,23 @@
+class AdminController < ApplicationController
+  def index
+    
+  end
+  
+  def locations
+    @locations = Location.find(:all)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @locations }
+    end
+  end
+  
+  def cities
+    @cities = City.find(:all)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @cities }
+    end
+  end
+end
