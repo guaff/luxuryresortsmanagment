3c1cce04f5f19ebc196ea0ef1c7762c2f31eebb3
Ivan Acosta-Rubio <ivan@bakedweb.net>
2009-02-02 16:33:33 -0500

    resfull auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 ActionController::Routing::Routes.draw do |map|
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users
+
+  map.resource :session
+
   map.resources :cities
 
   map.resources :locations
+  
+  map.home '', :controller => 'home', :action => 'index'
 
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090202134223) do
+ActiveRecord::Schema.define(:version => 20090202193430) do
+
+  create_table "cities", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "location_id"
+  end
 
   create_table "locations", :force => true do |t|
     t.string   "name"
@@ -18,5 +27,19 @@ ActiveRecord::Schema.define(:version => 20090202134223) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",                     :limit => 40
+    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
+  end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
 end
