988767a841de492a11661e633fd9f49387357bb6
Ivan Acosta-Rubio <ivan@bakedweb.net>
2009-02-24 13:31:28 -0500

    rails 2.2.2 in enviroment.rb

--- features/step_definitions/search_client.rb
+++ features/step_definitions/search_client.rb
@@ -1,32 +1,4 @@
 Given /^I am on the admin client page$/ do
-  @client = Client.create!(
-    :full_name => "ivan",
-    :full_name_second => "bellatrix",
-    :address => "our address 33132",
-    :city => "miami",
-    :state => "FL",
-    :zip => "33132",
-    :home_phone => "3053349348",
-    :work_phone => "30493094820",
-    :email => "ivan@bakedweb.net",
-    :fee_paid => "35000",
-    :payment_type => "cash",
-    :charge_date => "1-1-2002",
-    :credit_card => '1234567890',
-    :credit_card_type => 'visa',
-    :credit_card_expiration => '12/02',
-    :billing_name => "ivan",
-    :billing_address => "my address",
-    :billing_city => 'cuba',
-    :billing_state => 'HA',
-    :billing_zip => '33132',
-    :start_date => '1-1-2008',
-    :contract_return_date => '2-2-2007',
-    :cancellation_date => '2-2-3004',
-    :charge_return_date => '2-2-2004',
-    :tts_sale_person => 'pedro',
-    :resort_sale_person => 'pedro',
-    :notes => 'he is doing detox',
-    :client_pin => '2323')  
+  ivan = Factory(:client)
   visit '/admin/clients'
 end
--- test/factories.rb
+++ test/factories.rb
@@ -15,3 +15,34 @@ Factory.define :user do |u|
   u.email 'ivan@bakedweb.net'
   u.password_confirmation '01210323'
 end
+
+Factory.define :client do |u|
+    u.full_name  "ivan"
+    u.full_name_second  "bellatrix"
+    u.address  "our address 33132"
+    u.city  "miami"
+    u.state "FL"
+    u.zip "33132"
+    u.home_phone "3053349348"
+    u.work_phone "30493094820"
+    u.email "ivan@bakedweb.net"
+    u.fee_paid "35000"
+    u.payment_type "cash"
+    u.charge_date "1-1-2002"
+    u.credit_card '1234567890'
+    u.credit_card_type 'visa'
+    u.credit_card_expiration '12/02'
+    u.billing_name "ivan"
+    u.billing_address "my address"
+    u.billing_city 'cuba'
+    u.billing_state 'HA'
+    u.billing_zip '33132'
+    u.start_date '1-1-2008'
+    u.contract_return_date '2-2-2007'
+    u.cancellation_date '2-2-3004'
+    u.charge_return_date '2-2-2004'
+    u.tts_sale_person 'pedro'
+    u.resort_sale_person 'pedro'
+    u.notes 'he is doing detox'
+    u.client_pin '2323'
+end
