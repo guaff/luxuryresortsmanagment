87128a29b698d8cd18355b38ee5d198d145167cf
Ivan Acosta-Rubio <ivan@Macintosh-2.local>
2009-04-16 11:11:53 -0400

    settings for sending email in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,3 +19,8 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+# Don't care if the mailer can't send
+config.action_mailer.raise_delivery_errors = true
+
+# set delivery method to :smtp, :sendmail or :test
+config.action_mailer.delivery_method = :smtp
