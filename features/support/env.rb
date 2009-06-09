a26cfaf3c89f56be06d24fffaea08cde11ddbba6
Ivan Acosta-Rubio <ivan@bakedweb.net>
2009-06-09 12:08:17 -0400

    upgrading to rails 2.3.2

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,7 +10,17 @@ Cucumber::Rails.use_transactional_fixtures
 require 'webrat'
 
 Webrat.configure do |config|
-  config.mode = :rails
+  config.mode = :selenium
+  # Selenium defaults to using the selenium environment. Use the following to override this.
+  # config.application_environment = :test
+end
+
+# this is necessary to have webrat "wait_for" the response body to be available
+# when writing steps that match against the response body returned by selenium
+World(Webrat::Selenium::Matchers)
+
+Before do
+  # truncate your tables here, since you can't use transactional fixtures*
 end
 
 # Comment out the next two lines if you're not using RSpec's matchers (should / should_not) in your steps.
