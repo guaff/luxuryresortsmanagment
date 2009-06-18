592f57f739d0f7cc1df9dff459543c2dc445557e
Ivan Acosta-Rubio <ivan@bakedweb.net>
2009-06-18 12:43:13 -0400

    contracts

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,17 +10,7 @@ Cucumber::Rails.use_transactional_fixtures
 require 'webrat'
 
 Webrat.configure do |config|
-  config.mode = :selenium
-  # Selenium defaults to using the selenium environment. Use the following to override this.
-  # config.application_environment = :test
-end
-
-# this is necessary to have webrat "wait_for" the response body to be available
-# when writing steps that match against the response body returned by selenium
-World(Webrat::Selenium::Matchers)
-
-Before do
-  # truncate your tables here, since you can't use transactional fixtures*
+  config.mode = :rails
 end
 
 # Comment out the next two lines if you're not using RSpec's matchers (should / should_not) in your steps.
